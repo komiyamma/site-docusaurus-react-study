0dd0b55079aa826f96ab6519ac6b490c97a03203
komiyamma <komiyama0629@gmail.com>
2026-01-25 02:30:38 +0900

    fix(website): projectName をリポジトリ名に修正

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'komiyamma', // Usually your GitHub org/user name.
-  projectName: 'react-study.komiyamma.net', // Usually your repo name.
+  projectName: 'site-docusaurus-react-study', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
